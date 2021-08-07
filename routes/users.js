4d2405f50df69a8ded1bcd2c338ab5c448db6a3f
KHH1124 <kkmo1124@naver.com>
2021-08-07 16:04:16 +0900

    use mysql

--- routes/users.js
+++ routes/users.js
@@ -1,78 +1,96 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  connectionLimit : 10,
+  host : '127.0.0.1',
+  user : 'root',
+  password : '^^rla940794',
+  database : 'userEx'
+})
 
 // 회원가입
 router.post('/signup', function(req, res, next) {
-  const user = req.body;
-  let newDb;
-  fs.readFile('database.json', 'utf8', (err,data) => {
-    if(data) {
-      let originDb = JSON.parse(data)
-      let arrayDb = originDb.users ? originDb.users : []
-      for(let x of arrayDb) {
-        if(user.id == x.id) return res.json('user exist')
+  const body = req.body
+  pool.getConnection((err, connection) =>{
+    if(err) throw err;
+    
+    connection.query('SELECT id FROM users WHERE id = ?', body.id,(error, results, fields) => {
+      // connection.release();
+      if(results.length == 0) {
+        connection.query(`INSERT INTO users SET ?`, body, (error, results, fields) => {
+          connection.release();
+          console.log(results)
+          res.status(200).json({results})
+          if(error) throw error;
+        })
+      } else {
+        return res.status(200).json('exist id')
       }
-      arrayDb.push(user)
-      originDb.users = arrayDb
-      newDb = JSON.stringify(originDb)
-    } else {
-      newDb = JSON.stringify({users: [user]})
-    }
-    fs.writeFile('database.json', newDb, 'utf8', (err) => {
-      res.status(200).json({result:true, list: newDb})
+      if(error) throw error;
     })
+    
   })
 })
 
 //로그인
 router.post('/signin', function(req, res, next) {
   const {id, pwd} = req.body
-  fs.readFile('database.json', 'utf8', (err,data) => {
-    let originDb = JSON.parse(data)
-    let arrayDb = originDb.users
-    for(let x of arrayDb) {
-      if(id == x.id && pwd == x.pwd) {
+  pool.getConnection(function(err, connection) {
+    if(err) throw err;
+    connection.query('SELECT id, pwd FROM users WHERE id = ?', id,(error, results, fields) => {
+      connection.release();
+      console.log(results)
+      console.log(id, pwd, results[0].id, results[0].pwd)
+      if(id == results[0].id && pwd == results[0].pwd) {
         return res.status(200).json({login:true})
       }
-    }
-    res.status(200).json('wrong id or password')
+      res.status(200).json('wrong id or password')
+      if(error) throw error;
+    })
   })
 })
 
 //회원 정보 수정
 router.put('/', function(req, res, next) {
-  const { name, id, pwd, idx}  = req.body
-  const user = {name:name, id:id, pwd:pwd}
-  fs.readFile('database.json', 'utf8', (err,data) => {
-    let originDb = JSON.parse(data)
-    originDb.users[idx] = user
-    let newDb=JSON.stringify(originDb)
-    fs.writeFile('database.json', newDb, 'utf8', (err) => {
-      res.status(201).json({result: true, list: newDb})
+  const body = req.body;
+  pool.getConnection((err, connection) => {
+    if(err) throw err;
+    connection.query(`UPDATE users SET ? WHERE idx = ?`, [body, body.idx], (error, results, fields) => {
+      connection.release();
+      console.log(results)
+      res.status(200).json({results})
+      if(error) throw error;
     })
   })
 })
 
 //회원 탈퇴
 router.delete('/', function(req, res, next) {
-  const {idx} = req.body
-  fs.readFile('database.json', 'utf8', (err, data) => {
-    let originDb = JSON.parse(data)
-    delete originDb.users[idx]
-    originDb.users = originDb.users.filter(x=>x!=null)
-    let newDb = JSON.stringify(originDb)
-    fs.writeFile('database.json', newDb, 'utf8', (err) => {
-      res.status(201).json({result: true, list: newDb})
+  const body = req.body
+  pool.getConnection(function(err,connection) {
+    if(err) throw err;
+    connection.query(`DELETE FROM users WHERE idx = ?`, body.idx,(error, results, fields) => {
+      connection.release();
+      console.log(results)
+      res.status(200).json({results})
+      if(error) throw error
     })
   })
 })
 
 //회원 확인
 router.get('/', function(req, res, next) {
-  fs.readFile('database.json', 'utf8', (err, data) => {
-    let Db = JSON.parse(data)
-    res.status(200).json({result: true, list: Db.users})
+  pool.getConnection(function(err, connection) {
+    if(err) throw err;
+    connection.query('SELECT * FROM users', (error, results, fields) => {
+      connection.release();
+      console.log(results)
+      res.status(200).json({results})
+      if(error) throw error;
+    })
   })
 });
 
